5fabe7e834c432088be8795a9a501add307bdc14
Zeishan <53163911+zeishan-hasan@users.noreply.github.com>
2023-03-01 08:40:00 +0500

    Ownership of string variables

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,23 @@
-/* This code will cause compile time error since here
- * we are trying to modify an immnutable variable.
- */ 
-/* 
 fn main() {
-    let x = 5;
-    println!("The value of x is: {x}");
-    // Modifying x viz. an immutable variable
-    x = 9;
-    println!("The value of x is: {x}");
+    let string_1 = gives_ownership();
+    let string_2 = String::from("Welcome");
+    let stirng_3 = takes_and_gives_back(string_2);
+
+    println!("{}", string_1);
+
+    // This will cause error since string_2 is moved to 'takes_and_gives_back()'
+    // and string type does not have a copy trait.
+    // println!("{}", string_2);
+    
+    println!("{}", stirng_3);
+
 }
-*/
 
-/* This code will run fine since the updated variable 
- * is mutable.
- */
-/*
- fn main() {
-    let mut var: i32 = 7;
-    println!("The value of var is: {var}");
-    // Modifying var viz. mutable
-    var = 9;
-    println!("var is updated to: {var}");
+fn gives_ownership() -> String {
+    let some_string = String::from("This is from gives_ownership().");
+    some_string
 }
-*/
 
-/* Demonstration of constants. */
-/*
-fn main() {
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-    println!("THREE_HOURS_IN_SECONDS: {THREE_HOURS_IN_SECONDS}");
-}
-*/
-
-/* Demonstration of SHADOWING. */
-/*
-fn main() {
-    let x = 5;
-    let x = x + 1;
-    println!("The value of x before entering the inner scope is: {x}");
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
-
-    println!("The value of x after exiting the inner scope is: {x}");
-}
-*/
-
-fn main() {
-    /* This fashion of code will make the first immutable variable
-     * unused and will result in compilation warning with a suggestion
-     * to use '_' (_immutable_var) before the var name which is a convention to indaicate
-     * the compiler that the variable is intentionally unused.
-     */
-
-/*  let immutable_var: u32 = 7;
-    let immutable_var: u32 = 9;
-
-    println!("immutable_var: {immutable_var}");
-*/
-
-    /* This fashion will let you redefine the immuatable variable.
-     * Notice the use of 'let' for reassigning the immutable variable;
-     * it's in contrast to the mutable varibale where a variable can
-     * be redefined without the use of 'let' 
-     */
-    let immutable_var: u32 = 7;
-    println!("immutable_var: {immutable_var}");
- 
-    let immutable_var: u32 = 9;
-    println!("immutable_var: {immutable_var}");
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
